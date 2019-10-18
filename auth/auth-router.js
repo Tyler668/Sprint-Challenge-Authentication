080aa9f4037117362768a4b3f9a39d4cf34e31e6
Tyler Gilchrist <tyler668@gmail.com>
2019-10-18 11:47:56 -0400

    auth middleware instituted

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model.js');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js')
+
 
 router.post('/register', (req, res) => {
   // implement registration
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,26 @@
   before granting access to the next middleware/route handler
 */
 
+
+
+
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js')
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+      } else {
+        req.user = {
+          username: decodedToken.username,
+          department: decodedToken.department
+        };
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ message: 'No credentials provided, you shall not pass!' });
+  }
 };
